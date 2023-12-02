31c2cc404cb7fb100083ff62085f882fa62911b3
omeramasib <omeramasib999@gmail.com>
2023-12-02 21:46:15 +0300

    add unit test

---
+++ littlelemon/tests/test_views.py
@@ -0,0 +1,7 @@
+# from django.test import TestCase
+# from restaurant.models import Menu
+
+# class MenuViewTest(TestCase):
+#     def setUp(self):
+#         item = Menu.objects.create(Title="IceCream", Price=80, Inventory=100)
+#         self.assertEqual(item, Menu.objects.get(Title="IceCream"))
