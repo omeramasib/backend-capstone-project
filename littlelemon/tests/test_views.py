ff2429626438bf0e8c38e8fe81a2e0466a4caa30
omeramasib <omeramasib999@gmail.com>
2023-12-02 22:10:49 +0300

    add test for MenueViewTest

--- littlelemon/tests/test_views.py
+++ littlelemon/tests/test_views.py
@@ -1,7 +1,10 @@
-# from django.test import TestCase
-# from restaurant.models import Menu
+from django.test import TestCase
+from restaurant.models import Menu
 
-# class MenuViewTest(TestCase):
-#     def setUp(self):
-#         item = Menu.objects.create(Title="IceCream", Price=80, Inventory=100)
-#         self.assertEqual(item, Menu.objects.get(Title="IceCream"))
+class MenuViewTest(TestCase):
+    def setUp(self):
+        self.Menu = Menu.objects.create(Title="IceCream", Price=80, Inventory=100)
+
+
+    def test_get_item(self):
+        self.assertIsInstance(self.Menu.Title, str)
